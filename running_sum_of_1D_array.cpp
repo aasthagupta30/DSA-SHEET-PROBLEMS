66f0f8456da510225673dcb92dc50af8b9a0c243
AASTHA GUPTA <72187301+aasthagupta30@users.noreply.github.com>
2022-07-24 12:18:17 +0530

    Create running_sum_of_1D_array.cpp
    leetcode ques

---
+++ running_sum_of_1D_array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int sum=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            nums[i]=sum;
+        }
+        return nums;
+    }
+};
